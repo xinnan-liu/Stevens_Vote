8abf29e365a7813d248e0d94701e237deafec1c1
Senon-Liu <shinnon.ryu@gmail.com>
2014-02-02 10:58:37 -0500

    log

---
+++ SmartVote/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<title>Vote Site</title>
+	</head>
+<body>
+	<div class="container wrapper">
+		<div class="row-fluid">
+			
+			<ul class="unstyled">
+				<li>
+					<div class="row-fluid">
+						<div class="span2">
+							span2
+						</div>
+						<div class="span6">
+							span6
+						</div>
+						<div class="span6 offset2">
+							span6
+						</div>
+					</div>
+				</li>
+			</ul>
+
+ 
+
+		</div>
+	
+	</div>
+
+	
+	
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
